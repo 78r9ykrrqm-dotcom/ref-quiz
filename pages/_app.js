af9f9b726f8c48cfeb6d37f90cef03cee799996e
Benjamin Shifrin <benjaminshifrin@Benjamins-MacBook-Air.local>
2026-07-12 16:32:10 +0300

    Add site footer

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,14 @@ export default function App({ Component, pageProps }) {
   return (
     <div className="site-shell">
       <Navigation />
+
       <main className="page-shell">
         <Component {...pageProps} />
       </main>
+
+      <footer className="site-footer">
+        © 2026 Benjamin Shifrin. All Rights Reserved.
+      </footer>
     </div>
   );
 }
